a64ea68c0631ecf5c9952a0d0049271ef6acc1b3
Subhankarnandi777 <subhankarnandi2006@gmail.com>
2026-03-19 18:55:34 +0530

    removed all files

--- for_loop.cpp
+++
@@ -1,5 +0,0 @@
-int teacups = 5;
-for (int i = 0; i <= teacups; i++)
-{
-    cout << "brewing a cup" << i << "of tea..." << endl;
-}
--- ternery_operator.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    int n = 45;
-
-    cout << (n >= 0 ? "positive" : "negetive");
-    return 0;
-}
