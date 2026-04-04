54fc4bc5e5f748acb21ec37b26aa1595b598cf7c
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-04-04 19:47:33 +0530

    Add files via upload

---
+++ basics/bitwise_AND.cpp
@@ -0,0 +1,9 @@
+  #include <iostream>
+    using namespace std;
+
+    int main(){
+        int a = 4 , b= 8; // a = 0100 , b = 1000
+
+        cout << (a & b) << endl; // 0
+        return 0;
+    }
