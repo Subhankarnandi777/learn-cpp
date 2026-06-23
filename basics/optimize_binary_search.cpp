ba91ced675e19a08076e732ac0fcd1da19c5b261
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-06-23 09:20:37 +0530

    Add files via upload

---
+++ basics/optimize_binary_search.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int binarySearch (vector <int> arr , int tar){
+    int st = 0 , end = arr.size() - 1;
+
+    while (st <= end){
+        int mid = st + (end - st)/ 2;
+
+
+        if (tar > arr[mid]){
+            st = mid + 1;
+        } else if (tar < arr[mid]){
+            end = mid - 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+int main(){
+    vector <int> arr = {0,2,4,6,8,9};
+    int tar = 6;
+
+    cout << binarySearch (arr , tar) << endl;
+    return 0;
+}
