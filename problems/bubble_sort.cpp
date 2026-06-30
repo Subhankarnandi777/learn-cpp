6afb819b774464faa54136481b2c1c833941fe09
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-06-30 13:41:23 +0530

    Optimize bubblesort with early termination
    Added a flag to optimize bubblesort by stopping if no swaps occur.

--- problems/bubble_sort.cpp
+++ problems/bubble_sort.cpp
@@ -4,10 +4,15 @@ using namespace std;
 
 void bubblesort (int arr[] , int n){
     for (int i = 0 ; i < n -1 ; i++){
+        bool isSwap = false;
         for (int j = 0; j < n-i-1 ; j++){
             if (arr[j] > arr[j+1]){
                 swap (arr[j] , arr[j+1]);
             }
+        }
+
+        if (!isSwap) {
+            return;
         }
     }
 }
