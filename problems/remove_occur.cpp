867df667652030b418ffa5822529449921ae04eb
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-07-17 19:36:33 +0530

    Add files via upload

---
+++ problems/remove_occur.cpp
@@ -0,0 +1,28 @@
+/*
+Given two strings s and part, perform the following operation on s until all occurrences of the substring part are removed:
+
+Find the leftmost occurrence of the substring part and remove it from s.
+Return s after removing all occurrences of part.
+
+A substring is a contiguous sequence of characters in a string.
+*/
+ 
+
+#include <iostream>
+using namespace std;
+
+string removeOccurances(string s, string part){
+    while(s.length()!=0 && s.find(part)< s.length()){
+        s.erase(s.find(part),part.length());
+    }
+    return s;
+}
+int main(){
+    string s, part;
+    cout<<"Enter a String: ";
+    getline(cin,s);
+    cout<<"Enter a Substring: ";
+    getline(cin,part);
+    cout<<"Result: "<< removeOccurances(s,part) <<endl;
+    return 0;
+}
