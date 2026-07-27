2e91ce0e26b1435890e4a6994dcc17215ea31869
Subhankar Nandi <subhankarnandi2006@gmail.com>
2026-07-27 12:54:25 +0530

    Implement optimalDiagonalSum function for matrix
    Added an optimized function to calculate the sum of diagonal elements in a matrix.

--- problems/diagonalSum.cpp
+++ problems/diagonalSum.cpp
@@ -2,7 +2,7 @@
 #include <climits>
 using namespace std;
 
-int diagonalSum (int mat[][4], int n){
+int diagonalSum (int mat[][4], int n){ // time complexity = O(n^2)
     int sum = 0;
     for(int i = 0 ; i < n ; i++){
         for(int j = 0 ; j < n ; j++){
@@ -15,11 +15,22 @@ int diagonalSum (int mat[][4], int n){
     }
     return sum;
 }
+int optimalDiagonalSum(int mat[][4], int n){ // optimized approach to find the sum of diagonal elements (O(n))
+    int sum = 0;
+    for(int i = 0 ; i < n ; i++){
+        sum += mat[i][i];
+        if(i != n-i-1){
+            sum += mat[i][n-i-1];
+        }
+    }
+    return sum;
+}
+
 
 int main(){
     int matrix[4][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
     int n = 4;
 
-    cout << diagonalSum(matrix , n) << endl;
+    cout << optimalDiagonalSum(matrix , n) << endl;
     return 0;
 }
